23343982e10298cc1a6b52c8853c4eafafdd94ac
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-07-07 13:11:44 -0300

    ItemPedido e ItemPedidoPK - Parte 1

--- src/main/java/com/nelioalves/domain/Produto.java
+++ src/main/java/com/nelioalves/domain/Produto.java
@@ -2,7 +2,9 @@ package com.nelioalves.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -34,12 +36,24 @@ public class Produto implements Serializable {
 			   inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {}
 
 	public Produto(Integer id, String nome, Double preco) {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	//método que lista todos os pedidos associados a um produto
+	public List<Pedido> getPedidos() {
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido ip : itens) {
+			lista.add(ip.getPedido());
+		}
+		
+		return lista;
 	}
 
 	public Integer getId() {
@@ -72,6 +86,14 @@ public class Produto implements Serializable {
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
