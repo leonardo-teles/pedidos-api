8be34e0699b552ca34616200d4149a6aee1652b6
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-07-29 14:53:18 -0300

    Implementando SmtpEmailService com Servidor do Google

--- src/main/java/com/nelioalves/config/DevelopmentConfig.java
+++ src/main/java/com/nelioalves/config/DevelopmentConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.service.BDService;
+import com.nelioalves.service.EmailService;
+import com.nelioalves.service.SmtpMailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevelopmentConfig {
 		bdService.instanciarBDTeste();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpMailService();
 	}
 }
--- src/main/java/com/nelioalves/service/SmtpMailService.java
+++ src/main/java/com/nelioalves/service/SmtpMailService.java
@@ -2,16 +2,21 @@ package com.nelioalves.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
-public class MockEmailService extends AbstractEmailService {
-
+public class SmtpMailService extends AbstractEmailService {
+	
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
+
+	@Autowired
+	private MailSender mailSender;
 	
 	@Override
 	public void enviarEmail(SimpleMailMessage msg) {
-		LOG.info("Simulando envio de e-Mail...");
-		LOG.info(msg.toString());
+		LOG.info("Enviando e-Mail...");
+		mailSender.send(msg);
 		LOG.info("e-Mail enviado");
 	}
 }
